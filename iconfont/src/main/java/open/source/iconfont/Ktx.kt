d6d0f7a02aa0b1500c2c04b6e7ca78faf4f39d95
luotinghui <luotinghui@bilibili.com>
2021-12-07 01:48:13 +0800

    update

--- iconfont/src/main/java/open/source/iconfont/Ktx.kt
+++ iconfont/src/main/java/open/source/iconfont/Ktx.kt
@@ -2,14 +2,15 @@ package open.source.iconfont
 
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import androidx.annotation.DrawableRes
 import androidx.annotation.XmlRes
 import androidx.appcompat.content.res.AppCompatResources
 
-fun Context.getAppCompatDrawable(@XmlRes @DrawableRes resId: Int) {
-    AppCompatResources.getDrawable(this, resId)
+fun Context.getAppCompatDrawable(@XmlRes @DrawableRes resId: Int): Drawable? {
+    return AppCompatResources.getDrawable(this, resId)
 }
 
-fun Resources.getAppCompatDrawable(@XmlRes @DrawableRes resId: Int) {
-    AppCompatResources.getDrawable(InflateContext.obtain(this), resId)
+fun Resources.getAppCompatDrawable(@XmlRes @DrawableRes resId: Int): Drawable? {
+    return AppCompatResources.getDrawable(InflateContext.obtain(this), resId)
 }
