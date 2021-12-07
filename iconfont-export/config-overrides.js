cd315417a592b6809a4d528b9c743d17f0df9bea
luotinghui <luotinghui@bilibili.com>
2021-12-08 01:23:03 +0800

    更新导出

--- iconfont-export/config-overrides.js
+++
@@ -1,9 +0,0 @@
-const paths = require('react-scripts/config/paths');
-const path = require('path');
-module.exports = function override(config, env) {
-    paths.appBuild = path.join(paths.appBuild, 'app');
-    if (config.output.path){
-        config.output.path = path.join(config.output.path, 'app');
-    }
-    return config;
-};
